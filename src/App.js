3d181b535df46f0b0691ffce681838edcb30f251
martinacruz <cruz.s.martin@gmail.com>
2022-01-03 19:51:50 -0800

    create repo

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React  from 'react'
+import './App.css';
+import NavBar from './Components/NavBar'
+import Banner from './Components/Banner'
+import SectionGrid from './Components/SectionGrid';
+
+function App() {
+
+  return (
+    <div className="App">
+      <NavBar />
+      <Banner />
+      <SectionGrid />
+    </div>
+  );
+}
+
+export default App;
