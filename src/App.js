d0431d3752b508742290b3ef6dde620a0211f6f8
Martin Cruz <cruz.a.martin@gmail.com>
2022-03-23 13:48:04 -0700

    add new favicon, complete about page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import "./App.css";
 import NavBar from "./Components/NavBar";
 import Details from "./pages/Details";
 import Home from "./pages/Home";
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,44 @@
 import React from "react";
+import "../styles/About.css";
+import gif from "../assets/about-gif.gif";
 
 const About = () => {
   return (
-    <div>
-      <h1>About Page</h1>
+    <div className="about-content">
+      <img src={gif} />
+      <div className="about-info">
+        <p>
+          This website was made Martin Cruz as a solo project.
+          <br /> My goal was to take a team project made during my bootcamp and
+          redesign the front end. You can find the original repo{" "}
+          <a
+            href="https://github.com/trefynwynd/AniShowList-frontend"
+            target="_blank"
+          >
+            here
+          </a>{" "}
+          and credit to the team members. Some components from the original repo
+          were reused.
+          <br /> The design of the page was a complete attempt at cloning a
+          similar website created by{" "}
+          <a href="https://animedia.netlify.app/" target="_blank">
+            Vitor Trimer
+          </a>
+          <br /> This project uses the{" "}
+          <a href="https://jikan.moe/" target="_blank">
+            Jikan
+          </a>{" "}
+          free API{" "}
+        </p>
+      </div>
+      <div className="links">
+        <h3>Lets connect:</h3>
+        <p>
+          <a href="https://www.linkedin.com/in/martincrz/">Linkedin </a> |
+          <a href="https://martinc.dev/">Portfolio </a> |
+          <a href="https://github.com/martinacruz">Github</a>
+        </p>
+      </div>
     </div>
   );
 };
